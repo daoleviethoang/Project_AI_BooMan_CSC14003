e06af45fc4f346b8f9869970600f1efdbd51bd6b
Hoàng Xuân Kiệt <48423316+18127124@users.noreply.github.com>
2020-08-14 11:05:28 +0700

    Update readfile.py

--- src/readfile.py
+++ src/readfile.py
@@ -28,18 +28,17 @@ def readfile(path):
 def check_fence(map, size, start):
     new_map = map
     flag = 0
-    row_one = np.ones(size[0], dtype=int)
-    col_one = np.ones(size[1], dtype=int).reshape((size[1], 1))
+    row_one = np.ones(size[1], dtype=int).reshape((1, size[1]))
+    col_one = np.ones(size[0], dtype=int).reshape((size[0], 1))
 
-    first_row = map[0:1,]
+    first_row = map[0:1,:]
     # t = (first_row == row_one)
     # for i in t:
     #     if i != True:
     #         flag-=1
     if np.array_equal(first_row, row_one):
-        flag-=1
-    else:
-        flag += 1
+        flag+=1
+    
 
     last_row = map[size[0] - 1:,]
     # t = (last_row == row_one)[0]
@@ -47,9 +46,8 @@ def check_fence(map, size, start):
     #     if i != True:
     #         flag-=1
     if np.array_equal(last_row,row_one):
-        flag -= 1
-    else:
         flag += 1
+    
 
     first_col = map[:,0:1]
     # t = (first_col == col_one)[0]
@@ -57,8 +55,8 @@ def check_fence(map, size, start):
     #     if i != True:
     #         flag-=1
     if np.array_equal(first_col, col_one):
-        flag -= 1
-    flag += 1
+        flag += 1
+    
 
     last_col = map[:, size[1] -1 :]
     # t = (last_col == col_one)[0]
@@ -66,13 +64,14 @@ def check_fence(map, size, start):
     #     if i != True:
     #         flag-=1
     if np.array_equal(last_col, col_one):
-        flag -= 1
-    flag+=1
+        flag += 1
+    
 
     if flag != 4:
         #them hang2 rao
-        row_one = np.ones(size[0]).reshape((1, size[0]))
-        col_one = np.ones(size[1] + 2).reshape((size[1] + 2, 1))
+        row_one = np.ones(size[1], dtype=int).reshape((1, size[1]))
+        col_one = np.ones(size[0] + 2, dtype=int).reshape((size[0] + 2, 1))
+        
         new_map = np.r_[row_one, new_map]  #add row
         new_map = np.r_[new_map, row_one]  
         new_map = np.c_[new_map, col_one]   #add collumn
@@ -84,10 +83,12 @@ def check_fence(map, size, start):
         return map, size, start
 
 if __name__ == "__main__":
-    size, map, start = readfile('input/2/input1.txt')
+    size, map, start = readfile('input/level2.txt')
+    
     size = np.array(size)
     map = np.array(map)
     start = np.array(start)
+    
     map, size, start = check_fence(map, size, start)
     print(map)
     print(size)
